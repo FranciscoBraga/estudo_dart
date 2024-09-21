275a8610f766b71d6bce4f1e8f4b0051128e7ab5
FranciscoBraga <franciscobraga.fob@gmail.com>
2024-09-21 18:13:08 -0400

    Operadores lógicos e estrutura condicionais

--- bin/meu_app.dart
+++ bin/meu_app.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
   print('Hello world: ${meu_app.calculate()}!');
   //tipos de váriavies
-
+/* variáveis
 print("--------------------------números--------------------------------------------");
 /*   int numero = 10;
 
@@ -245,5 +248,40 @@ print(data1);
 var data2 = DateTime.parse("2022-02-01 00:00:00");
 print(data1.isAfter(data2));
 print(data1.isBefore(data2));
-print(data1.compareTo(data2));
+print(data1.compareTo(data2)); */
+
+//escrevendo no terminal
+
+  print("Informe a primeira nota");
+  var line = stdin.readLineSync(encoding: utf8);
+  var prova1 = int.parse(line ?? "0");// é como se fosse um if ternário com null  se for nulo oega zero senão ele mesmo
+
+  print("Informe a segunda nota");
+  line = stdin.readLineSync(encoding: utf8);
+  var prova2 = int.parse(line ?? "0");
+//operadores condicionais
+
+  var media = (prova1 + prova2) / 2;
+
+  print(media);
+  print(media >= 7);
+  print(media < 7);
+  print(prova1 == prova2);
+  print(prova1 != prova2);
+  print((prova1 >= 7) && (prova2 >= 7));
+  print((prova1 == 10) || (prova2 == 10));
+
+// if
+  if (media >= 7) {
+    print("O aluno passou, com a nota: " + media.toString());
+  } else if (media >= 5) {
+    print("O aluno está de recuperação, com a nota: $media");
+  } else {
+    print("O aluno passou, com a nota: $media");
+  }
+
+  //if ternário
+  var resultado = (media >= 7) ? "O aluno passou" : "O aluno reprovou";
+
+  print(resultado);
 }
